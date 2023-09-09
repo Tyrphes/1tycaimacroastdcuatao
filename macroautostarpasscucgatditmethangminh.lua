ca37da46f7bc6609ef8763cd229f5d522b4ce984
Tyrphes <98525287+Tyrphes@users.noreply.github.com>
2023-09-09 08:05:51 +0700

    Update macroautostarpasscucgatditmethangminh.lua

--- macroautostarpasscucgatditmethangminh.lua
+++ macroautostarpasscucgatditmethangminh.lua
@@ -18,13 +18,14 @@ local args29 = {
     [2] = true
 }
 game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Input"):FireServer(unpack(args29))
-repeat game:GetService("RunService").RenderStepped:Wait() until game.Players.LocalPlayer.Money.Value >= 1000
+task.wait(0.5)
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Input"):FireServer(unpack(args29))
 local args464 = {
     [1] = "Summon",
     [2] = {
         ["Rotation"] = 0,
-        ["cframe"] = CFrame.new(1226.97, 903.227, -691.133, 1, 0, 0, 0, 1, 0, 0, 0, 1),
-        ["Unit"] = "W3Hudron"
+        ["cframe"] = CFrame.new(1216.79, 917.746, -1709.51, 1, 0, 0, 0, 1, 0, 0, 0, 1),
+        ["Unit"] = "Ivankov2"
     }
 }
 
@@ -37,28 +38,17 @@ game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Input
         end
 
 
-repeat game:GetService('RunService').RenderStepped:Wait() until game.Players.LocalPlayer.Money.Value >= 3000
-local args201 = {
-    [1] = "Upgrade",
-    [2] = DeployedUnit[1]
-}
-game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Server"):InvokeServer(unpack(args201))
-repeat game:GetService('RunService').RenderStepped:Wait() until game.Players.LocalPlayer.Money.Value >= 5000
-local args201 = {
-    [1] = "Upgrade",
-    [2] = DeployedUnit[1]
-}
-game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Server"):InvokeServer(unpack(args201))
-repeat game:GetService('RunService').RenderStepped:Wait() until game.Players.LocalPlayer.Money.Value >= 15000
-local args201 = {
-    [1] = "Upgrade",
-    [2] = DeployedUnit[1]
-}
-game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Server"):InvokeServer(unpack(args201))
-repeat game:GetService('RunService').RenderStepped:Wait() until game.Players.LocalPlayer.Money.Value >= 50000
+repeat game:GetService('RunService').RenderStepped:Wait() until game.Players.LocalPlayer.Money.Value >= 2130
 local args201 = {
     [1] = "Upgrade",
     [2] = DeployedUnit[1]
 }
 game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Server"):InvokeServer(unpack(args201))
 
+repeat game:GetService('RunService').RenderStepped:Wait() until game.ReplicatedStorage.WaveValue.Value == 15
+wait(2)
+local args201 = {
+    [1] = "Sell",
+    [2] = DeployedUnit[1]
+}
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Server"):InvokeServer(unpack(args201))
